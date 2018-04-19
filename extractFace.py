eb03afa22bcbbbdf80424f4ad50e7489f05912bc
Ioannis Kansizoglou <38525506+IoannisKansizoglou@users.noreply.github.com>
2018-04-19 14:51:00 +0300

    Add files via upload

---
+++ extractFace.py
@@ -0,0 +1,280 @@
+###################### LIBRARIES ######################
+
+import cv2
+import numpy as np
+import os, os.path, time
+import pandas as pd
+
+##################### PARAMETERS #####################
+
+# Starting time
+t0 = time.time()
+# Threshold for keeping a center movement per frame
+c_threshold = 100
+# Threshold for keeping a center vs other
+d_threshold = 100000
+# Values for first cropping
+y1, y2, x1, x2, x3, x4 = (150, 300, 80, 300, 430, 650)
+# Final size of cropped image
+width, height = (96, 96)
+
+
+###################### FUNCTIONS #####################
+
+
+### Function for creating folders ###
+def create_folder(PATH):
+    
+    try:
+        if not os.path.exists(PATH):
+            os.makedirs(PATH)
+    except OSError:
+        print ('Error: Creating directory' + PATH)
+
+
+### Function to crop image tracking the face ###
+def crop_image(image, left_precenter, right_precenter, sheet, speaker):
+
+    # Set face tracking type
+    cascPATH = '/home/gryphonlab/Ioannis/Works/IEMOCAP/CodeFaceDetection/haarcascade_frontalface_alt2.xml'
+    face_cascade = cv2.CascadeClassifier(cascPATH)
+    # First crop of image to simplify face-detection
+    img = image.copy()
+    img1 = img[y1:y2, x1:x2]
+    img2 = img[y1:y2, x3:x4]
+    gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    # Detect faces (-eyes) in the image
+    left_faces = face_cascade.detectMultiScale(gray1, scaleFactor=1.01, minNeighbors=5)
+    right_faces = face_cascade.detectMultiScale(gray2, scaleFactor=1.01, minNeighbors=5)
+    # Track left speaker
+    left_center, left_index = select_window(left_faces, left_precenter)
+    # Track right speaker
+    right_center, right_index = select_window(right_faces, right_precenter)
+    # Calculate left crop rectangle
+    print(left_center)
+    left_a = left_center[0] - int(width/2)
+    left_b = left_center[1] - int(height/2)
+    # Draw proposed left center, face rectangle and crop rectangle
+    cv2.circle(img1,tuple(left_center),2,(0,255,0),2)
+    if left_index >= 0:
+        x, y, w, h = left_faces[left_index]
+        cv2.rectangle(img1,(x,y),(x+w,y+h),(255,0,0),2)    
+    cv2.rectangle(img1,(left_a,left_b),(left_a+width,left_b+height),(0,0,255),2)
+    # Calculate right crop rectangle
+    print(right_center)
+    right_a = right_center[0] - int(width/2)
+    right_b = right_center[1] - int(height/2)
+    # Draw proposed left center, face rectangle and crop rectangle
+    cv2.circle(img2,tuple(right_center),2,(0,255,0),2)
+    if right_index >= 0:
+        x, y, w, h = right_faces[right_index]
+        cv2.rectangle(img2,(x,y),(x+w,y+h),(255,0,0),2)    
+    cv2.rectangle(img2,(right_a,right_b),(right_a+width,right_b+height),(0,0,255),2)
+    # Concatenate left and right images
+    img = np.concatenate((img1, img2), axis=1)
+    # Show results
+    cv2.imshow('Image',img)
+    cv2.waitKey(1)
+    # Select speaker to crop according to extractionmap.xlsx
+    if speaker == 'L':
+        
+        # Crop image
+        left_a += x1
+        left_b += y1
+        image = image[left_b:left_b+width, left_a:left_a+height]
+        
+    elif speaker == 'R':
+
+        # Crop image
+        right_a += x3
+        right_b += y1
+        image = image[right_b:right_b+width, right_a:right_a+height]
+        
+    else:
+
+        image = np.array([0, 0])
+
+    # Return cropped image and next precenters
+    return image, left_center, right_center
+
+
+### Function to find current speaker ###
+def find_speaker(frame, row, xl, sheet):
+
+    # Create DataFrame from current excel's sheet
+    df = xl.parse(sheet)
+    # Check current frame's position in xlsx row
+    if frame < df['iframe'][row]:
+        speaker = 'None'
+    elif frame <= df['fframe'][row]:
+        speaker = df['speaker'][row]
+    else:
+        speaker = 'None'
+        if (df.index == row+1).any():
+            row += 1
+    
+    return speaker, row
+
+
+### Function to select the best window before cropping
+def select_window(faces, precenter):
+
+    # Case[1] of detecting no 'face'
+    if np.shape(faces)[0] == 0:
+        # Proposed center
+        center = precenter
+        # False value for index in faces of selected window
+        index = -1
+    
+    else:
+
+        # Index in faces of selected window
+        index = 0
+        # Case[2] of detecting many 'faces'
+        if np.shape(faces)[0] > 1:
+            
+            # Starting default distance
+            dmin = d_threshold
+            i = 0
+
+            # Decide which to keep
+            for (x,y,w,h) in faces:
+
+                # Compute center
+                xc = int(round((2*x+w)/2))
+                yc = int(round((2*y+h)/2))
+                d = np.linalg.norm(np.array([xc, yc])-np.array(precenter))
+                # Change appropriately min and index
+                if d < dmin:
+                    dmin = d
+                    index = i
+
+                i += 1
+
+            # Take values for proposed center
+            index = int(index)
+            x, y, w, h = faces[index]
+
+        # Case[3] of detecting exactly one 'face'
+        else:
+
+            # Take values for proposed center
+            x, y, w, h = faces[0]
+            xc = int(round((2*x+w)/2))
+            yc = int(round((2*y+h)/2))
+            dmin = np.linalg.norm(np.array([xc, yc])-np.array(precenter))
+            
+        # Proposed centre
+        xc = int(round((2*x+w)/2))
+        yc = int(round((2*y+h)/2))
+        # Check distance with precenter threshold
+        if dmin < c_threshold:
+            # Proposed center is accepted
+            center = [xc, yc]
+        else:
+            # Proposed center is discarded, keep precenter
+            center = precenter
+
+        #cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        # Compute big axis of face-detection rectangle
+        #s1 = np.array([x, y])
+        #s2 = np.array([x+w, y+h])
+        #print(np.linalg.norm(s1-s2))
+
+    if precenter == [0, 0]:
+        x, y, w, h = faces[0]
+        xc = int(round((2*x+w)/2))
+        yc = int(round((2*y+h)/2))
+        center = [xc, yc]
+
+    return center, index
+
+
+
+######################## MAIN ########################
+
+
+def main():
+
+    # Folder to store all .jpg files
+    imagePATH = '/home/gryphonlab/Ioannis/Works/IEMOCAP/InputFaces'
+    create_folder(imagePATH)
+
+    # For printing effect
+    counter = 1
+
+    for ses in range(1,6):
+
+        # CHANGE THIS if dataset IEMOCAP is moved
+        sessionPATH = '/media/gryphonlab/8847-9F6F/Ioannis/IEMOCAP_full_release/Session'+str(ses)+'/dialog/avi/DivX'
+        videos = os.listdir(sessionPATH)
+
+        # Current session folder
+        image_sessionPATH = imagePATH + '/Session' + str(ses)
+        create_folder(image_sessionPATH)
+
+        # Path to session's excel file
+        extractionmapPATH = '/home/gryphonlab/Ioannis/Works/IEMOCAP/Core/cut_extractionmap'+str(ses)+'.xlsx'
+        # Read excel file
+        xl = pd.ExcelFile(extractionmapPATH)
+
+        # First precenters don't exist
+        left_precenter = [0, 0]
+        right_precenter = [0, 0]
+
+        for vid in range(3,len(videos)):
+
+            # Current video path
+            videoPATH = sessionPATH + '/' + str(videos[vid])
+
+            # Current sheet name
+            sheet = videos[vid][:-4]
+            print(sheet)
+
+            print(str(counter)+ ') Creating video...' + videoPATH)
+            counter += 1
+
+            if os.path.isfile(videoPATH):
+
+                # Folder for frames of current video
+                image_videoPATH = image_sessionPATH + '/' + videos[vid]
+                create_folder(image_videoPATH)
+                # Playing video from file
+                cap = cv2.VideoCapture(videoPATH)
+                # First frame0.jpg
+                currentFrame = 0
+                # Row of extractionmap to search according to currentFrame
+                currentRow = 0
+                # Capture frame-by-frame
+                success, frame = cap.read()
+
+                while success:
+
+                    # Find current speaker
+                    currentSpeaker, currentRow = find_speaker(currentFrame, currentRow, xl, sheet)
+                    # Crop frame
+                    frame, left_precenter, right_precenter = crop_image(frame, left_precenter, right_precenter, sheet, currentSpeaker)
+                    # Check that current frame refers to speaker
+                    if np.shape(frame)[0]>2:
+                        # Save current cropped image in .jpg file
+                        name = str(image_videoPATH) + '/frame' + str(currentFrame) + '.jpg'
+                        cv2.imwrite(name, frame)
+                    # To stop duplicate images
+                    print('Current Frame: Frame'+str(currentFrame))
+                    currentFrame += 1
+                    # Try capture next frame
+                    success, frame = cap.read()
+
+        
+        # When everything is done, release the capture
+        cap.release()
+        cv2.destroyAllWindows()
+        print('Done"')
+
+        # Execution time
+        print( 'Execution time of extractFace.py [sec]: ' + str(time.time() - t0) )
+
+# Control runtime
+if __name__ == '__main__':
+    main()
---
+++ extractSpectrogram.py
@@ -0,0 +1,214 @@
+
+########################### LIBRARIES ##########################
+
+import numpy as np
+import os, os.path, time, subprocess
+from scipy.io import wavfile
+from scipy.signal import butter, lfilter
+import scipy.ndimage
+import pandas as pd
+
+########################## PARAMETERS ##########################
+
+# Starting time
+t0 = time.time()
+# Number of Sessions
+n_sessions = 4
+# Video's frame per second
+video_rate = 30
+# Window size for the FFT
+fft_size = 192
+# Distance to slide along the window
+step_size = fft_size/13.8
+# Threshold for spectrograms (lower filters out more noise)
+spec_threshold = 4
+# Low cut for the butter bandbass filter (Hz)
+lowcut = 500
+# High cut for the butter bandbass filter (Hz)
+highcut = 15000
+
+########################### FUNCTIONS ##########################
+
+
+### Function for creating folders ###
+def create_folder(PATH):
+
+    try:
+        if not os.path.exists(PATH):
+            os.makedirs(PATH)
+    except OSError:
+        print ('Error: Creating directory' + PATH)
+
+
+# Functions for the butter filter
+def butter_bandbass(lowcut, highcut, fs, order=5):
+
+    nyq = 0.5*fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+def butter_bandbass_filter(data, lowcut, highcut, fs, order=5):
+
+    b, a = butter_bandbass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+
+# Function to create an overlapped version of X
+def overlap(X, window_size, window_step):
+
+    if window_size%2 != 0:
+        raise ValueError('Window size must be even!!')
+    #Make sure there is an even number of windows before stridetricks
+    append = np.zeros((window_size - len(X)%window_size))
+    X = np.hstack((X,append))
+
+    ws = window_size
+    ss = window_step
+    a = X
+
+    valid = len(a) - ws
+    nw = int((valid)//ss)
+    out = np.ndarray((nw,ws), dtype=a.dtype)
+
+    for i in range(nw):
+        # "slide" the window along the samples
+        start = int(i*ss)
+        stop = int(start+ws)
+        out[i] = a[start : stop]
+
+    return out
+
+
+# Function to compute Short-Time Fourier Transform (STFT)
+def stft(X, fftsize=128, step=65, mean_normalize=True, real=False, compute_onesided=True):
+
+    if real:
+        local_fft = np.fft.rfft
+        cut = -1
+    else:
+        local_fft = np.fft.fft
+        cut = None
+    
+    if compute_onesided:
+        cut = int(fftsize // 2)
+    
+    if mean_normalize:
+        X -= X.mean()
+
+    X = overlap(X, fftsize, step)
+
+    size = fftsize
+    win = 0.54 - 0.46 * np.cos(2*np.pi*np.arange(size)/(size-1))
+    X = X*win[None]
+    X = local_fft(X)[:, :cut]
+    return X
+
+
+# Function to create spectrogram
+def pretty_spectrogram(d, log=True, thresh=5, fft_size=512, step_size=64):
+
+    specgram = np.abs(stft(d, fftsize=fft_size, step=step_size, real=False, compute_onesided=True))
+
+    if log == True:
+        # Normalize volume to max 1
+        specgram /= specgram.max()
+        # Take log
+        specgram = np.log10(specgram)
+        # Set anything less than threshold equal to threshold
+        specgram[specgram < -thresh] = -thresh
+    else:
+        # Set anything less than threshold equal to threshold
+        specgram[specgram < thresh] = thresh
+
+    return specgram
+
+
+############################# MAIN #############################
+
+def main():
+
+    # Folder to store all the spectrograms (.txt files)
+    spectrogramPATH = '/home/gryphonlab/Ioannis/Works/IEMOCAP/InputSpectrograms'
+    create_folder(spectrogramPATH)
+
+    # Number of sessions to iterate
+    for ses in range(2,n_sessions+2):
+        
+        # Path to session's excel file
+        extractionmapPATH = '/home/gryphonlab/Ioannis/Works/IEMOCAP/Core/cut_extractionmap'+str(ses)+'.xlsx'
+        # Read excel file
+        xl = pd.ExcelFile(extractionmapPATH)
+        # List of excel's sheet names
+        sheets = xl.sheet_names
+        # Path of current session's spectrograms 
+        spectrogramPATH = spectrogramPATH+'/Session'+str(ses)+'/'
+        create_folder(spectrogramPATH)
+        # Directory of mono-wav files
+        wavPATH = '/media/gryphonlab/8847-9F6F/Ioannis/IEMOCAP_full_release/Session'+str(ses)+'/dialog/monowav/'
+        ###create_folder(wavPATH)
+        #videos = os.listdir(sessionPATH)
+        
+        for sheet in sheets:
+
+                
+            # Current video path
+            ###videoPATH = sessionPATH + '/' + str(vid)
+            # Current audio file name
+            fileNAME = sheet+'.wav'
+            # Path of current video's spectrograms
+            spectrogramPATH = spectrogramPATH+fileNAME+'/'
+            create_folder(spectrogramPATH)
+            # Extract mono audio-file (.wav) from video-file (.avi)
+            ###command = 'ffmpeg -i '+videoPATH+' -ab 160k -ac 1 -ar 44100 -vn '+wavPATH+fileNAME
+            ###subprocess.call(command, shell=True)
+            # Open wav file
+            audio_rate, data = wavfile.read(wavPATH+fileNAME)
+            # Filter the audio file
+            #data = butter_bandbass_filter(data, lowcut, highcut, audio_rate, order=1)
+            # Extract spectrogam frames according to video frames
+
+            # Create DataFrame from current excel's sheet
+            sheet_df = xl.parse(sheet)
+            # Copy iframe to numpy array
+            iframe = np.array(sheet_df['iframe'])
+            # Copy fframe to numpy array
+            fframe = np.array(sheet_df['fframe'])
+
+            for i in range(0,np.shape(iframe)[0]):
+
+                for frame in range(iframe[i], fframe[i]+1):
+                    
+                    # Take data simultaneous with current video frame
+                    framedata = data[int(frame*audio_rate/video_rate):int((frame+1)*audio_rate/video_rate)]
+                    if np.shape(framedata)[0] == 1470:
+                        # Compute spectrogram array for current frame
+                        wav_spectrogram = pretty_spectrogram(framedata.astype('float64'), fft_size=fft_size, step_size=step_size, \
+                                                                log=True, thresh=spec_threshold)
+                        # Save spectrogram array to .txt file
+                        #np.savetxt(spectrogramPATH+'frame'+str(frame)+'.txt', wav_spectrogram, fmt='%1.5f')
+                        # Save to binary
+                        wav_spectrogram *= -10000
+                        np.save(spectrogramPATH+'frame'+str(frame), wav_spectrogram.astype('uint16'))
+                        #wav_spectrogram.astype('uint16').tofile(spectrogramPATH+'frame'+str(frame)+'.txt')
+                        #df = pd.DataFrame(wav_spectrogram).astype('float16')
+                        #df.to_excel(spectrogramPATH+'frame'+str(frame)+'.xlsx')
+
+            # Return back to path of current session's spectrograms 
+            spectrogramPATH = '/home/gryphonlab/Ioannis/Works/IEMOCAP/InputSpectrograms/Session'+str(ses)+'/'
+
+        # Return back to InputSpectograms folder
+        spectrogramPATH = '/home/gryphonlab/Ioannis/Works/IEMOCAP/InputSpectrograms'
+        print('End of Session: '+str(ses))
+                    
+
+        
+    # Execution time
+    print( 'Execution time of extractSpectrogram.py [sec]: '+ str(time.time() - t0) )
+
+
+# Control runtime
+if __name__ == '__main__':
+    main()
